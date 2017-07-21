7c0a493ceade5961d3ad881d70899579f51bd303
dave lane <david.lane@blueprism.com>
2017-07-21 14:21:48 +0100

    Old Exercices

---
+++ ex2.py
@@ -0,0 +1,9 @@
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by Python.
+
+print "I could use code like this." # and this comment after is ignored
+
+# You can also use a comment to "disable" or comment out a piece of code:
+# print "This Won't run."
+
+print "This will run."
