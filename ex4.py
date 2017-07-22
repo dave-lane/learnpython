8f6125b65a77c55af8e7f31fffb1721a2ec4295e
dave lane <david.lane@blueprism.com>
2017-07-22 17:41:35 +0100

    Adding ex5 & Drill

--- ex4.py
+++ ex4.py
@@ -1,10 +1,18 @@
+#Define how many cars we have
 cars = 100
+#How much space is in each car
 space_in_a_car = 4.0
+#How many drivers we have today
 drivers = 30
+#How many passengers are there today
 passengers = 90
+#How many cars are left over (not enough drivers)
 cars_not_driven = cars - drivers
+#Cars with drivers
 cars_driven = drivers
+#The total space we have today in all cars
 carpool_capacity = cars_driven * space_in_a_car
+#How many passengers will have to be in each car on average to all get transported
 average_passengers_per_car = passengers / cars_driven
 
 
